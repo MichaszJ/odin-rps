8c5b2aaba5d54a9aa4da434e45a81efdbcf3fad8
MichaszJ <mjski@live.ca>
2022-08-31 13:26:14 -0400

    implemented play round function

--- index.js
+++ index.js
@@ -3,14 +3,29 @@ function getComputerChoice() {
     let choice = '';
 
     if (choiceInt == 0) {
-        choice = 'Rock'
+        choice = 'rock';
     } else if (choiceInt == 1) {
-        choice = 'Paper'
+        choice = 'paper';
     } else {
-        choice = 'Scissors'
-    }
+        choice = 'scissors';
+    };
 
     return choice
 };
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection = getComputerChoice()) {
+    let result = '';
+
+    if ((playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors') || (playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock') || (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper')) {
+        result = `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else if (playerSelection.toLowerCase() == computerSelection) {
+        result = `It's a tie! You both chose ${playerSelection}`;
+    }
+    else {
+        result = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    };
+
+    return result;
+};
+
+console.log(playRound('rock'));
